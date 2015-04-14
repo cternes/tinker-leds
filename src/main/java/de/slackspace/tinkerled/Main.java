b570eae46b28ecf9bc54afdb5f4d5256da51d764
cternes <github@slackspace.de>
2015-04-14 17:54:55 +0200

    Wrapped in spring boot application

--- src/main/java/de/slackspace/tinkerled/Main.java
+++ src/main/java/de/slackspace/tinkerled/Main.java
@@ -3,19 +3,28 @@ import java.io.IOException;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
+	private Log logger = LogFactory.getLog(getClass());
+	
 	public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+		SpringApplication.run(Main.class, args);
+	}
+	
+	public Main() {
 		LedStripManager mgr = new LedStripManager();
 		boolean connected = mgr.connect();
 		
 		if(connected) {
+			logger.debug("Successfully connected to tinkerforge brick");
+			
 			mgr.run();
-			
-			System.out.println("Press key to exit"); System.in.read();
-			
-			mgr.disconnect();
 		}
 	}
 }
